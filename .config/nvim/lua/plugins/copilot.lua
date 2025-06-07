deebda49afb140f8ee912ed6d6b2fb852d457913
Edgar Gonzalez <edgargdev@gmail.com>
2025-06-07 14:57:04 -0500

    change pdf viewer for vimtex to skim. add keybinds

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -7,7 +7,7 @@ return {
 		vim.keymap.set("i", "<A-j>", 'copilot#Accept("<CR>")', { expr = true, replace_keycodes = false })
 		vim.keymap.del("i", "<Tab>")
 
-		vim.g.copilot_no_tab_map = true
+		-- vim.g.copilot_no_tab_map = true
 		vim.g.copilot_assume_mapped = true
 		vim.keymap.set("i", "<A-w>", "<Plug>(copilot-accept-word)")
 		vim.keymap.set("i", "<A-n>", "<Plug>(copilot-next)")
--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -4,7 +4,14 @@ return {
 	-- tag = "v2.15", -- uncomment to pin to a specific release
 	init = function()
 		-- VimTeX configuration goes here, e.g.
-		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_view_method = "skim"
+		vim.g.vimtex_view_skim_sync = 1
+		vim.g.vimtex_view_skim_activate = 1
 		vim.g.vimtex_syntax_enabled = 0
+
+		-- set key mapping for :VimtexView
+		vim.api.nvim_set_keymap("n", "<leader>lv", ":VimtexView<CR>", { noremap = true, silent = true })
+		-- set key mapping for :VimtexTocOpen
+		vim.api.nvim_set_keymap("n", "<leader>lt", ":VimtexTocOpen<CR>", { noremap = true, silent = true })
 	end,
 }
