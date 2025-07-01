1be84e0bd68718ad40fc659b5953474c2de1a18f
Edgar Gonzalez <edgargdev@gmail.com>
2025-07-01 09:41:43 -0500

    add close other "files" keymap. add gitsigns keymaps

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -11,3 +11,24 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- Delete all listed buffers except the ones currently visible in any tab
+local function delete_buffers()
+	-- Get all visible buffers in all tabs
+	local visible_bufs = {}
+	for tab = 1, vim.fn.tabpagenr("$") do
+		local tab_bufs = vim.fn.tabpagebuflist(tab)
+		for _, b in ipairs(tab_bufs) do
+			visible_bufs[b] = true
+		end
+	end
+
+	-- Delete non-visible listed buffers
+	for bnr = 1, vim.fn.bufnr("$") do
+		if vim.fn.buflisted(bnr) == 1 and not visible_bufs[bnr] then
+			vim.cmd("bd " .. bnr)
+		end
+	end
+end
+
+vim.keymap.set("n", "<leader>z", delete_buffers, { silent = true, desc = "Delete all non-windowed buffers" })
