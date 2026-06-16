568018aadc43aebeace1fd41b3396ee2deb6c7ee
Edgar Gonzalez <edgargdev@gmail.com>
2026-06-16 16:57:37 -0500

    update autocomplete to work with sql dadbodui

--- .config/nvim/lua/plugins/vim-dadbod.lua
+++ .config/nvim/lua/plugins/vim-dadbod.lua
@@ -1,14 +1,38 @@
 return {
-	{ "tpope/vim-dadbod" },
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		event = "VeryLazy",
-		config = function()
-			vim.keymap.set("n", "<leader>db", "<cmd>DBUI<CR>", { desc = "Open DB UI" })
-		end,
+	"kristijanhusak/vim-dadbod-ui",
+	dependencies = {
+		{ "tpope/vim-dadbod", lazy = true },
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
 	},
-	{
-		"kristijanhusak/vim-dadbod-completion",
-		event = "VeryLazy",
+	cmd = {
+		"DBUI",
+		"DBUIToggle",
+		"DBUIAddConnection",
+		"DBUIFindBuffer",
 	},
+	keys = {
+		{ "<leader>db", "<cmd>DBUI<CR>", desc = "Open DB UI" },
+	},
+	init = function()
+		vim.g.db_ui_win_position = "right"
+
+		-- Register the dadbod completion source for sql buffers (buffer-local).
+		-- Note: the cmp source name is "vim-dadbod-completion" (with hyphens).
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "sql", "mysql", "plsql" },
+			callback = function()
+				local ok, cmp = pcall(require, "cmp")
+				if not ok then
+					return
+				end
+				cmp.setup.buffer({
+					sources = {
+						{ name = "vim-dadbod-completion" },
+						{ name = "luasnip" },
+						{ name = "path" },
+					},
+				})
+			end,
+		})
+	end,
 }
