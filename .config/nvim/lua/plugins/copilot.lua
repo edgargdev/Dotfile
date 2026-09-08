2d28d85df8b452acad6982caa23daeb111da0458
Edgar Gonzalez <edgargdev@gmail.com>
2026-09-08 11:52:17 -0500

    update copilot lua

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,14 +1,19 @@
 return {
 	"github/copilot.vim",
+	cmd = "Copilot",
 	event = "InsertEnter",
+	init = function()
+		vim.g.copilot_enabled = false
+		vim.g.copilot_no_tab_map = true
+	end,
+	keys = {
+		{ "<leader>ce", "<cmd>Copilot enable<CR>", desc = "Enable Copilot" },
+		{ "<leader>cd", "<cmd>Copilot disable<CR>", desc = "Disable Copilot" },
+	},
 	config = function()
-		vim.keymap.set("n", "<leader>ce", "<cmd>Copilot enable<CR>", { desc = "Enable Copilot" })
-		vim.keymap.set("n", "<leader>cd", "<cmd>Copilot disable<CR>", { desc = "Disable Copilot" })
-
 		vim.keymap.set("i", "<A-j>", 'copilot#Accept("<CR>")', { expr = true, replace_keycodes = false })
-		vim.keymap.del("i", "<Tab>")
+		pcall(vim.keymap.del, "i", "<Tab>")
 
-		-- vim.g.copilot_no_tab_map = true
 		vim.g.copilot_assume_mapped = true
 		vim.keymap.set("i", "<A-w>", "<Plug>(copilot-accept-word)")
 		vim.keymap.set("i", "<A-n>", "<Plug>(copilot-next)")
